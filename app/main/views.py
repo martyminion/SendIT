91a6b4c6ac979ea9fcf9bb6fa7d60805c197d50a
martyminion <martkimwaweru@gmail.com>
2020-05-13 20:30:20 +0300

    ft:email this enables anyone who wants to use email funtionality to easily call on the function and set up the parameter they need

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,24 @@
 from . import main
-from flask import render_template,request,url_for,flash
+from flask import render_template,request,url_for,flash,redirect
 from .. import db
+from ..email import mail_message
 
 
 @main.route('/')
 def index():
+
   
   return render_template('index.html')
+
+@main.route('/mailtest/')
+def test_email_parameters():
+  
+  mail_message("This is your receipt","email/receipt","martkimwaweru@gmail.com")
+  return redirect(url_for('main.index'))
+
+@main.route('/mailtest/moreparams')
+def test_email_parameters2():
+  
+  mail_message("Your parcel number 123","email/order","martkimwaweru@gmail.com")
+  
+  return redirect(url_for('main.index'))
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY="ghostrider"
+export MAIL_USERNAME='wawerulawrence@gmail.com'
+export MAIL_PASSWORD='gkjojwaeiyhrpurg'
 
 python3.6 manage.py server
